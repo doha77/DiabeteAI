df88f099db742c86dc422102e26f753bbfa2adc4
doha77 <81390440+doha77@users.noreply.github.com>
2023-01-20 21:32:53 +0100

    return prediction outcome

--- predict.py
+++ predict.py
@@ -1,5 +1,4 @@
 import pickle
-
 import numpy as np
 from pandas import DataFrame as df
 from models.patient import Patient
@@ -13,19 +12,19 @@ def predict(patients: df):
         input_data_as_numpy_array = np.asarray(patient)
         input_data_reshaped = input_data_as_numpy_array.reshape(1, -1)
         prediction = model.predict(input_data_reshaped)
+        patient['outcome'] = prediction[0]
         patients_classified = patients_classified.append(patient, ignore_index=True)
-        # TODO : add prediction as outcome in patients_classified
-        if prediction[0] == 0:
+        if prediction[0] == '0':
             print('not diabetic')
         else:
             print('diabetic')
 
-        return patients_classified
+    return patients_classified
 
 
 if __name__ == "__main__":
-    patients = []
-    patients.append(Patient(5, 166, 9, 964, 9, 6, 0.587, 44))
+    patients = [Patient(5, 166, 9, 964, 9, 6, 0.587, 44),
+                Patient(2, 197, 70, 45, 543, 30.5, 0.158, 53)]
     patients = df.from_records([p.to_dict() for p in patients])
     patients = patients.drop(columns='outcome', axis=1)
     predict(patients)
